298b24032cd7c6ff9fc9a0ac46c49592e2723057
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2020-12-08 17:31:04 +0200

    Fixed PR comments

--- server/dummy_flask_app.py
+++ server/dummy_flask_app.py
@@ -4,15 +4,6 @@ from flask_cors import CORS, cross_origin
 import threading
 from engineio.payload import Payload
 
-import os
-import sys
-
-SERVER_FOLDER = os.getcwd() + "/server"
-if SERVER_FOLDER not in sys.path:
-    sys.path.append(SERVER_FOLDER)
-
-from config import DEVICE
-
 Payload.max_decode_packets = 50
 
 import numpy as np
@@ -24,17 +15,19 @@ import json
 # root_folder_path = os.path.split(os.getcwd())[0] # cd .. to root
 print("root folder path", os.getcwd())
 
-sys.path.append(os.path.join(os.getcwd(), 'hrnet'))
-sys.path.append(os.path.join(os.getcwd(), 'server'))
+HRNET_FOLDER = os.path.join(os.getcwd(), 'hrnet')
+if HRNET_FOLDER not in sys.path:
+    sys.path.append(HRNET_FOLDER)
 
-sys.path.append(os.getcwd())
-# print(os.getcwd())
-from hrnet.SimpleHRNet import SimpleHRNet
+SERVER_FOLDER = os.path.join(os.getcwd(), 'server')
+if SERVER_FOLDER not in sys.path:
+    sys.path.append(SERVER_FOLDER)
+
+if os.getcwd() not in sys.path:
+    sys.path.append(os.getcwd())
+
 from server.image_prediction_queue import ImagePredictionQueue
 from server.reponses_queue import ResponsesQueue
-
-import torch
-import time
 
 app = Flask(__name__)
 # app.config['PORT'] = 8009
@@ -44,13 +37,6 @@ cors = CORS(app)
 
 # https://github.com/miguelgrinberg/python-engineio/issues/142
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent', ping_timeout=1000, ping_interval=1000)
-
-model = SimpleHRNet(48, 17, device=torch.device(DEVICE))
-
-# def queuecallback(prediction):
-#     dumped = json.dumps(prediction, cls=NumpyEncoder)
-#     socketio.emit('model did predict', {"prediction" : dumped})
-
 
 image_queue = ImagePredictionQueue()
 response_queue = ResponsesQueue(image_queue, socketio)
@@ -87,5 +73,5 @@ def send_video(socket):
 
 
 if __name__ == '__main__':
-    print("vova")
+    print("dummy flask app has started")
     socketio.run(app, port=8009)
